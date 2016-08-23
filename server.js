fbfd8d91f11b194d033b19c04035acade5e17cf8
pawel.bujna <pawel.bujna@btskyrise.com>
2016-08-23 22:43:46 +0200

    refactoring

--- server.js
+++ server.js
@@ -9,67 +9,60 @@ var app = express();
 var companiesNames = [];
 var companiesInfo = [];
 var companies = [];
+//
+// var createCompanyConstructor = function(companyName, companyInformation) {
+//   this.companyName = companyName;
+//   this.companyInformation = companyInformation;
+// };
+//
+// var saveFile = function() {
+//   stringify(companies, function(err, output){
+//     fs.writeFile('companiesList.csv', output, function (err) {
+//       if (!err) { console.log('File Saved') };
+//     });
+//   });
+// };
+//
+// var createCompanyObject =  function() {
+//   for (var i = 0; i < companiesNames.length; i++) {
+//     if (i < companiesNames.length - 1) {
+//       var company = new createCompanyConstructor(companiesNames[i], companiesInfo[i]);
+//       companies.push(company);
+//     } else {
+//       console.log("File is saving...");
+//       saveFile();
+//     }
+//   }
+// }
 
-//test
 
-var createCompanyConstructor = function(companyName, companyInformation) {
-  this.companyName = companyName;
-  this.companyInformation = companyInformation;
-};
-
-var saveFile = function() {
-  stringify(companies, function(err, output){
-    fs.writeFile('companiesList.csv', output, function (err) {
-      if (!err) { console.log('File Saved') };
-    });
-  });
-};
-
-var createCompanyObject =  function() {
-  for (var i = 0; i < companiesNames.length; i++) {
-    if (i < companiesNames.length - 1) {
-      var company = new createCompanyConstructor(companiesNames[i], companiesInfo[i]);
-      companies.push(company);
-    } else {
-      console.log("File is saving...");
-      saveFile();
-    }
-  }
-}
-
-var downloadData = function() {
-
-  var pagesQuantity = 1725;
-
+var loopThroughAllPages = function() {
+  var pagesQuantity = 17;
   for (var a = 0; a < pagesQuantity; a++) {
-    if (a < pagesQuantity) {
 
-      url = 'http://www.orf.pl/index.php?go=woj&woj=%9Cl%B9skie&a='+[a];
+    url = 'http://www.orf.pl/index.php?go=woj&woj=%9Cl%B9skie&a='+[a];
 
-      request({uri: url, encoding: null}, function(error, response, html) {
-
-        if (error) {return;};
-
-        html = iconv.decode(html, 'Windows-1250');
-
-        var $ = cheerio.load(html);
-
-        $('td.tresc > u:first-child').filter(function() {
-          var companyName = $(this).text();
-          companiesNames.push(companyName);
-        });
-
-        $('p.tresc').filter(function() {
-          var companyInfo = $(this).text();
-          companiesInfo.push(companyInfo);
-        });
-
-      });
-    } else {
-      // Wait until all data will load to array.
-      setTimeout(createCompanyObject, 180000);
-    };
+    request({uri: url, encoding: null}, function(error, response, html) {
+      if (error) { return; }
+      pullOutWebsiteData(html);
+    });
   };
+};
+
+var pullOutWebsiteData = function(html) {
+  var $ = cheerio.load(html);
+
+  $('td.tresc > u:first-child').filter(function() {
+    var companyName = $(this).text();
+    companiesNames.push(companyName);
+  });
+
+  $('p.tresc').filter(function() {
+    var companyInfo = $(this).text();
+    companiesInfo.push(companyInfo);
+  });
+
+  console.log(companiesNames.length);
 };
 
 downloadData()
